a5e288dbca018a600fa91b0c49dedb1620c69275
jalo <ali.aljalo@gmail.com>
2026-02-07 20:38:31 +0300

    Use ResponseEntity with proper HTTP status codes for Account API

--- src/main/java/com/example/taskmanager/controller/AccountController.java
+++ src/main/java/com/example/taskmanager/controller/AccountController.java
@@ -1,8 +1,9 @@
 package com.example.taskmanager.controller;
 
-
 import com.example.taskmanager.model.Account;
 import com.example.taskmanager.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,31 +18,36 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-//    @GetMapping
-//    public Account getAccount(){
-//        return accountService.getAccount();
-//    }
+    //GET all
     @GetMapping
-    public List<Account> getAllAccounts(){
-        return accountService.getAllAccounts();
+    public ResponseEntity<List<Account>> getAllAccounts(){
+        return ResponseEntity.ok(accountService.getAllAccounts());
     }
 
+    //GET by id
     @GetMapping("/{id}")
-    public Account getAccountById(@PathVariable Long id){
-        return accountService.getAccountById(id);
+    public ResponseEntity<Account> getAccountById(@PathVariable Long id){
+        return ResponseEntity.ok(accountService.getAccountById(id));
     }
 
+    //CREATE
     @PostMapping
-    public Account createAccount(@RequestBody Account account){
-        return accountService.createAccount(account);
+    public ResponseEntity<Account> createAccount(@RequestBody Account account){
+        Account createAccount = accountService.createAccount(account);
+        return new ResponseEntity<>(createAccount, HttpStatus.CREATED);
     }
 
+    //UPDATE
     @PutMapping("/{id}")
-    public Account updateAccount(@PathVariable Long id, @RequestBody Account account){
-        return accountService.updateAccount(id, account);
+    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account account){
+        Account update =  accountService.updateAccount(id, account);
+        return ResponseEntity.ok(update);
     }
-    @DeleteMapping("/{id")
-    public void deleteAccount(@PathVariable Long id){
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id){
         accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
     }
 }
